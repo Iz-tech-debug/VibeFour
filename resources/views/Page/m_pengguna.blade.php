0acea10728a2b36a1eb94a42b362d99e7cc3f878
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-02-11 20:06:55 +0700

    Penambahan halaman manajemen berita dan juga manajemen pengguna

---
+++ resources/views/Page/m_pengguna.blade.php
@@ -0,0 +1,21 @@
+@extends('dashboard')
+
+@section('title', 'Manajemen Pengguna')
+
+@section('content')
+
+    <div class="container mt-4">
+
+        <div class="card p-3 shadow-sm">
+            <h4 class="mt-2" style="color:blueviolet;">Manajemen Pengguna</h4>
+        </div>
+
+        <br>
+
+        <div class="card p-4 shadow-sm">
+
+        </div>
+
+    </div>
+
+@endsection
