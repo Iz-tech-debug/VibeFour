24386320f9de4e80ac5a646fef04d6a351ad505c
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-02-11 20:07:59 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,20 @@ Route::get('/dashboard', function () {
     return view('home');
 });
 
+
 // Route::get('/editor_halaman', function () {
 //     return view('Page_Editor.editor_halaman');
 // });
 
 // Pindah atau ambil data dari editor halaman
 Route::get('/editor_halaman', function () {
-    return view('Page_Editor.editor_halaman'); // Sesuaikan nama file Blade Anda
+    return view('Page_Editor.editor_halaman');
 })->name('editor.halaman');
+
+Route::get('/manajemen_berita', function () {
+    return view('Page.m_berita');
+})->name('page.m_berita');
+
+Route::get('/manajemen_pengguna', function () {
+    return view('Page.m_pengguna');
+})->name('page.m_pengguna');
