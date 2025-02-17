15382ae58466112bdff9d4bc1b90749eaedb67e7
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-02-17 09:23:36 +0700

    Penambahan File modal dan controller

---
+++ app/Http/Controllers/editor_halaman/BerandaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\editor_halaman;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BerandaController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
