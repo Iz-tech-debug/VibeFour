50d605b68c3c6cba12e1dd13d9b97e81b5dca730
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-02-17 16:00:20 +0700

    Pembuatan halaman tentang

--- resources/views/Page_Editor/Sections/tentang.blade.php
+++ resources/views/Page_Editor/Sections/tentang.blade.php
@@ -1,129 +1,154 @@
-{{-- SA --}}
+@extends('dashboard')
 
-@include('Modal.Editor.sa')
+@section('title', 'Editor Halaman - Tentang')
 
-<form action="#" method="post">
-    @csrf
+@section('content')
 
-    {{-- Keterangan Singkat IDN --}}
-    <div class="mb-3">
-        <label for="keteranganIDN" class="form-label fw-bold">Keterangan Bahasa Indonesia Singkat:</label>
-        <input type="text" id="keteranganIDN" class="form-control is-invalid" placeholder="Ketik disini....."
-            required>
-        <div class="invalid-feedback">Keterangan tidak boleh kosong.</div>
-    </div>
+    <div class="container mt-4">
 
-    {{-- Keterangan Singkat ENG --}}
-    <div class="mb-3">
-        <label for="keteranganENG" class="form-label fw-bold">Keterangan Bahasa Inggris Singkat:</label>
-        <input type="text" id="keteranganENG" class="form-control is-invalid" placeholder="Ketik disini....."
-            required>
-        <div class="invalid-feedback">Keterangan tidak boleh kosong.</div>
-    </div>
-
-    {{-- Gambar Latar Belakang --}}
-    <div class="mb-3">
-        <label class="form-label fw-bold">Gambar Latar Belakang:</label>
-        <div class="mb-2">File Sekarang: <span class="text-warning">image/abc/def.jpg</span></div>
-        <input type="file" class="form-control" id="inputBackground" accept="image/*" required>
-        <img id="previewBackground" class="image-preview" src="#" alt="Pratinjau Latar Belakang"
-            style="display: none;">
-        <i class="bi bi-info-circle"></i>
-        <small class="text-muted">Tambahkan gambar dengan rasio 16:9</small>
-    </div>
-
-    <hr>
-
-    {{-- Deskripsi Perusahaan --}}
-    <div class="row mb-3">
-        <div class="col-md-6">
-            {{-- Deskripsi Perusahaan IDN --}}
-            <label for="editorDescIDN" class="form-label fw-bold">Deskripsi Identitas Perusahaan Bahasa
-                Indonesia:</label>
-            <textarea id="editorDescIDN" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
+        <div class="card p-3 shadow-sm">
+            <h4 class="mt-2" style="color:blueviolet;">Editor Halaman</h4>
         </div>
-        <div class="col-md-6">
-            {{-- Deskripsi Perusahaan ENG --}}
-            <label for="editorDescENG" class="form-label fw-bold">Deskripsi Identitas Perusahaan Bahasa Inggris:</label>
-            <textarea id="editorDescENG" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
-        </div>
-    </div>
 
-    <hr>
+        <br>
 
-    {{-- Misi Perusahaan --}}
-    <div class="row mb-3">
-        <div class="col-md-6">
-            {{-- Misi Perusahaan IDN --}}
-            <label for="editorMisIDN" class="form-label fw-bold">Misi Perusahaan Bahasa
-                Indonesia:</label>
-            <textarea id="editorDescIDN" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
-        </div>
-        <div class="col-md-6">
-            {{-- Misi Perusahaan ENG --}}
-            <label for="editorMisENG" class="form-label fw-bold">Misi Perusahaan Bahasa Inggris:</label>
-            <textarea id="editorDescENG" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
-        </div>
-    </div>
+        <div class="card p-4 shadow-sm">
 
-    <hr>
+            <div class="row mt-1">
+                <div class="col-md-9">
+                    <h5 class="mt-2">Tentang</h5>
+                </div>
+            </div>
 
-    {{-- Keunggulan Perusahaan --}}
-    <div class="row mb-3">
-        <label for="editorKeunggulan" class="form-label fw-bold">Keunggulan Perusahaan: </label>
+            <hr>
 
-        <div class="mt-2">
+            <form action="#" method="post">
+                @csrf
 
-            <div class="mt-2">
-
-                <button type="button" class="btn btn-success mb-2" onclick="addPencapaian()">
-                    <i class="bi bi-plus-lg"></i> Tambah Keunggulan
-                </button>
-
-                <div id="keunggulanContainer">
+                {{-- Keterangan Singkat IDN --}}
+                <div class="mb-3">
+                    <label for="keteranganIDN" class="form-label fw-bold">Keterangan Bahasa Indonesia Singkat:</label>
+                    <input type="text" id="keteranganIDN" class="form-control is-invalid" placeholder="Ketik disini....."
+                        required>
+                    <div class="invalid-feedback">Keterangan tidak boleh kosong.</div>
                 </div>
 
-            </div>
+                {{-- Keterangan Singkat ENG --}}
+                <div class="mb-3">
+                    <label for="keteranganENG" class="form-label fw-bold">Keterangan Bahasa Inggris Singkat:</label>
+                    <input type="text" id="keteranganENG" class="form-control is-invalid" placeholder="Ketik disini....."
+                        required>
+                    <div class="invalid-feedback">Keterangan tidak boleh kosong.</div>
+                </div>
+
+                {{-- Gambar Latar Belakang --}}
+                <div class="mb-3">
+                    <label class="form-label fw-bold">Gambar Latar Belakang:</label>
+                    <div class="mb-2">File Sekarang: <span class="text-warning">image/abc/def.jpg</span></div>
+                    <input type="file" class="form-control" id="inputBackground" accept="image/*" required>
+                    <img id="previewBackground" class="image-preview" src="#" alt="Pratinjau Latar Belakang"
+                        style="display: none;">
+                    <i class="bi bi-info-circle"></i>
+                    <small class="text-muted">Tambahkan gambar dengan rasio 16:9</small>
+                </div>
+
+                <hr>
+
+                {{-- Deskripsi Perusahaan --}}
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        {{-- Deskripsi Perusahaan IDN --}}
+                        <label for="editorDescIDN" class="form-label fw-bold">Deskripsi Identitas Perusahaan Bahasa
+                            Indonesia:</label>
+                        <textarea id="editorDescIDN" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
+                    </div>
+                    <div class="col-md-6">
+                        {{-- Deskripsi Perusahaan ENG --}}
+                        <label for="editorDescENG" class="form-label fw-bold">Deskripsi Identitas Perusahaan Bahasa
+                            Inggris:</label>
+                        <textarea id="editorDescENG" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
+                    </div>
+                </div>
+
+                <hr>
+
+                {{-- Misi Perusahaan --}}
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        {{-- Misi Perusahaan IDN --}}
+                        <label for="editorMisIDN" class="form-label fw-bold">Misi Perusahaan Bahasa
+                            Indonesia:</label>
+                        <textarea id="editorDescIDN" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
+                    </div>
+                    <div class="col-md-6">
+                        {{-- Misi Perusahaan ENG --}}
+                        <label for="editorMisENG" class="form-label fw-bold">Misi Perusahaan Bahasa Inggris:</label>
+                        <textarea id="editorDescENG" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
+                    </div>
+                </div>
+
+                <hr>
+
+                {{-- Keunggulan Perusahaan --}}
+                <div class="row mb-3">
+                    <label for="editorKeunggulan" class="form-label fw-bold">Keunggulan Perusahaan: </label>
+
+                    <div class="mt-2">
+
+                        <div class="mt-2">
+
+                            <button type="button" class="btn btn-success mb-2" onclick="addPencapaian()">
+                                <i class="bi bi-plus-lg"></i> Tambah Keunggulan
+                            </button>
+
+                            <div id="keunggulanContainer">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-end">
+                    <button type="button" class="btn btn-primary" id="btnSimpan">
+                        Simpan
+                    </button>
+                </div>
+
+            </form>
+
         </div>
+
     </div>
 
-    <div class="text-end">
-        <button type="button" class="btn btn-primary" id="btnSimpan">
-            Simpan
-        </button>
-    </div>
-
-</form>
-
-<script>
-    document.getElementById('btnSimpan').addEventListener('click', function() {
-        Swal.fire({
-            title: 'Apakah Anda yakin ingin menyimpan ini?',
-            text: 'Perubahan akan terjadi di website',
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Ya, Simpan',
-            cancelButtonText: 'Batal'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Jika dikonfirmasi, lakukan sesuatu
-                Swal.fire({
-                    title: 'Tersimpan!',
-                    text: 'Data Anda telah disimpan.',
-                    icon: 'success'
-                });
-            }
+    <script>
+        document.getElementById('btnSimpan').addEventListener('click', function() {
+            Swal.fire({
+                title: 'Apakah Anda yakin ingin menyimpan ini?',
+                text: 'Perubahan akan terjadi di website',
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Ya, Simpan',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Jika dikonfirmasi, lakukan sesuatu
+                    Swal.fire({
+                        title: 'Tersimpan!',
+                        text: 'Data Anda telah disimpan.',
+                        icon: 'success'
+                    });
+                }
+            });
         });
-    });
 
-    // Pencapaian
-    let keunggulan = 0;
+        // Pencapaian
+        let keunggulan = 0;
 
-    function addPencapaian() {
-        const container = document.getElementById('keunggulanContainer');
-        const html = `
+        function addPencapaian() {
+            const container = document.getElementById('keunggulanContainer');
+            const html = `
             <div class="row mt-3 justify-content-center">
                 <div class="col-md-1">
                     <button type="button" class="btn btn-danger" onclick="hapusKeunggulan(this)">
@@ -139,12 +164,14 @@
             </div>
         `;
 
-        container.insertAdjacentHTML('beforeend', html);
-        keunggulan++;
-    }
+            container.insertAdjacentHTML('beforeend', html);
+            keunggulan++;
+        }
 
-    function hapusKeunggulan(element) {
-        element.closest('.row').remove();
-        keunggulan--;
-    }
-</script>
+        function hapusKeunggulan(element) {
+            element.closest('.row').remove();
+            keunggulan--;
+        }
+    </script>
+
+@endsection
