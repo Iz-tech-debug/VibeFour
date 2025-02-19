b820f9b457df0d4954a1b4957ea36dbcbbdb79fa
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-02-19 21:45:43 +0700

    Perubahan tata letak editor bagian tnc

--- resources/views/Page_Editor/Sections/s&k.blade.php
+++ resources/views/Page_Editor/Sections/s&k.blade.php
@@ -4,10 +4,27 @@
 
 @section('content')
 
+    <style>
+        .ck-editor__editable {
+            min-height: 300px;
+        }
+    </style>
+
     <div class="container mt-4">
 
         <div class="card p-3 shadow-sm">
-            <h4 class="mt-2" style="color:blueviolet;">Editor Halaman</h4>
+            <div class="row">
+                <div class="col-md">
+                    <h4 class="mt-2" style="color:blueviolet;">Editor Halaman</h4>
+                </div>
+
+                <div class="col-md-3 mt-1 text-end">
+                    <select class="form-select" aria-label="Pilih Bahasa">
+                        <option value="1">Bahasa Indonesia</option>
+                        <option value="2">Bahasa Inggris</option>
+                    </select>
+                </div>
+            </div>
         </div>
 
         <br>
@@ -16,7 +33,7 @@
 
             <div class="row mt-1">
                 <div class="col-md-9">
-                    <h5 class="mt-2">Beranda</h5>
+                    <h5 class="mt-2">Syarat & Ketentuan - Bahasa Indonesia</h5>
                 </div>
             </div>
 
@@ -25,19 +42,11 @@
             <form action="#" method="post">
                 @csrf
 
-                {{-- Deskripsi S&K --}}
                 <div class="mb-3">
-                    {{-- Deskripsi S&K IDN --}}
-                    <label for="editorDescIDN" class="form-label fw-bold">Halaman Syarat & Ketentuan Bahasa
-                        Indonesia:</label>
+                    <label for="editorDescIDN" class="form-label fw-bold">Halaman Syarat & Ketentuan:</label>
                     <textarea id="editorDescIDN" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
                 </div>
 
-                <div class="mb-4">
-                    {{-- Deskripsi S&K ENG --}}
-                    <label for="editorDescENG" class="form-label fw-bold">Halaman Syarat & Ketentuan Bahasa Inggris:</label>
-                    <textarea id="editorDescENG" class="editor" rows="5" placeholder="Ketik disini....." required></textarea>
-                </div>
                 <div class="text-end">
                     <button type="button" class="btn btn-primary" id="btnSimpan">
                         Simpan
