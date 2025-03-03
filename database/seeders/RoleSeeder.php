74801dfa9135e82d65458554e2a529bad5f69abd
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-03-03 12:52:35 +0700

    Pengisian role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Seeder Role
+        $role = [
+            [
+                'nama_role' => 'Admin',
+            ],
+            [
+                'nama_role' => 'User',
+            ]
+        ];
+
+        foreach ($role as $value) {
+            Role::create([
+                'nama_role' => $value['nama_role'],
+            ]);
+        }
     }
 }
