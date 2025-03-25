e980c106c89114c8f0a7560c0867aa23d9a9a440
Daffa Faiz Alghozi <daffafaiz615@gmail.com>
2025-03-25 11:52:17 +0700

    Penambahan inputan untuk bagian editor halaman penjadwalan dan voting

--- app/Http/Controllers/editor_halaman/ProductController.php
+++ app/Http/Controllers/editor_halaman/ProductController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers\editor_halaman;
 
-use App\Models\Language;
+use App\Models\Vote;
 use App\Models\Price;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Models\Feature;
 use App\Models\Package;
+use App\Models\Product;
+use App\Models\Language;
+use App\Models\Schedule;
+use App\Models\Advantage;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -31,51 +39,251 @@ class ProductController extends Controller
         return view('Page_Editor.Sections.produk', compact('paket', 'bahasa', 'bahasa_id'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+
+    // Paket
+
+    public function Paket()
     {
-        //
+        $lang = Language::all();
+
+        $bahasa_id = 1;
+
+        $paket = Price::whereHas('features', function ($query) use ($bahasa_id) {
+            $query->where('packages.bahasa_id', $bahasa_id);
+        })->with([
+                    'features' => function ($query) use ($bahasa_id) {
+                        $query->where('packages.bahasa_id', $bahasa_id);
+                    }
+                ])->get();
+
+        return view('Page_Editor.Sections.Produk.biaya', compact('lang', 'paket'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function switchPaket($bahasaId)
     {
-        //
+
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function inputPaket()
     {
-        //
+        $lang = Language::all();
+
+        return view('Page_Editor.Sections.Produk.tambah_paket', compact('lang'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function addPaket(Request $request)
     {
-        //
+        $request->validate([
+            'nama_paket' => 'required|string|max:255',
+            'harga' => 'nullable|numeric',
+            'durasi' => 'required|integer',
+            'periode' => 'required|string',
+            'fitur' => 'required|array',
+            'fitur.*' => 'string|max:255',
+            'pilihBahasa' => 'required|exists:languages,id',
+        ]);
+
+        // Simpan harga ke tabel prices
+        $biaya = new Price();
+        $biaya->nama = $request->nama_paket;
+        $biaya->harga = $request->harga;
+        $biaya->durasi = $request->durasi;
+        $biaya->satuan_waktu = $request->periode;
+        $biaya->save(); // Simpan dulu agar ID tersedia
+
+        // Simpan fitur satu per satu dan ambil ID fitur pertama
+        $featureIds = [];
+        foreach ($request->fitur as $namaFitur) {
+            $fitur = Feature::create(['fitur' => $namaFitur]); // Simpan fitur
+            $featureIds[] = $fitur->id;
+        }
+
+        // Ambil fitur pertama sebagai default untuk packages
+        $firstFeatureId = $featureIds[0] ?? null;
+
+        // Simpan paket ke tabel packages (pastikan feature_id tidak kosong)
+        if ($firstFeatureId) {
+            $paket = new Package();
+            $paket->bahasa_id = $request->pilihBahasa;
+            $paket->price_id = $biaya->id;
+            $paket->feature_id = $firstFeatureId; // Isi dengan fitur pertama
+            $paket->save();
+        }
+
+        return redirect()->back()->with('success', 'Paket berhasil ditambahkan!');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function updatePaket(Request $request, $id)
     {
-        //
+        $request->validate([
+            'nama_paket' => 'required|string|max:255',
+            'harga' => 'required|numeric',
+            'durasi' => 'required|integer',
+            'periode' => 'required|string',
+        ]);
+
+        $paket = Price::findOrFail($id);
+        $paket->nama = $request->nama_paket;
+        $paket->harga = $request->harga;
+        $paket->durasi = $request->durasi;
+        $paket->satuan_waktu = $request->periode;
+        $paket->save();
+
+        return redirect()->back()->with('success', 'Paket berhasil diperbarui!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroyPaket($id)
     {
-        //
+        $paket = Package::findOrFail($id);
+        $paket = Price::findOrFail($paket->price_id);
+        $paket = Feature::findOrFail($paket->id);
+
+        $paket->delete();
+
+        return response()->json(['success' => 'Paket berhasil dihapus!']);
     }
+
+
+    // Voting
+    public function Voting()
+    {
+        $lang = Language::all();
+
+        // Ambil semua keunggulan berdasarkan bahasa
+        $advantages = Advantage::where('bahasa_id', 1)->get();
+
+        $data = Vote::where('bahasa_id', 1)
+            ->pluck('isi', 'nama');
+
+        // Ambil data kontak dengan bahasa default (misalnya bahasa dengan ID 1)
+        $bahasa_id = 1;
+
+        return view('Page_Editor.Sections.Produk.voting', compact('lang', 'bahasa_id', 'data', 'advantages'));
+    }
+
+    public function storeVote(Request $request, $bahasa)
+    {
+        // dd($request->all());
+        $fields = ['Judul', 'Deskripsi', 'TeksButtonCoba', 'TeksButtonTutorial', 'Link', 'JudulBagianKeunggulan', 'KeteranganBagianKeunggulan'];
+
+        $data = ['nama' => 'vote'];
+
+        // Cek apakah ada gambar baru
+        if ($request->hasFile('foto_produk')) {
+            $file = $request->file('foto_produk');
+            $filenameToStore = 'vote' . time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('images/product', $filenameToStore, 'public');
+            $data['gambar'] = $path;
+        }
+
+        Product::updateOrCreate(['nama' => 'vote'], $data);
+
+        foreach ($fields as $field) {
+            $value = $request->input($field, '');
+
+            Vote::updateOrCreate(
+                ['bahasa_id' => $bahasa, 'nama' => $field],
+                ['isi' => $value ?: '-']
+            );
+        }
+
+        // 🚀 Tambahkan keunggulan ke tabel `advantages`
+        if ($request->has('keunggulanV_judul')) {
+            foreach ($request->keunggulanV_judul as $index => $judul) {
+                $keunggulanData = [
+                    'nama' => $judul,
+                    'isi' => $request->keunggulanV_keterangan[$index] ?? '',
+                    'bahasa_id' => $bahasa,
+                ];
+
+                // Jika ada ikon yang diunggah
+                if ($request->hasFile("keunggulanV_image.$index")) {
+                    $file = $request->file("keunggulanV_image.$index");
+                    $filenameToStore = 'keunggulan_' . time() . '_' . $index . '.' . $file->getClientOriginalExtension();
+                    $path = $file->storeAs('images/advantages', $filenameToStore, 'public');
+                    $keunggulanData['ikon'] = $path;
+                }
+
+                // Simpan atau update keunggulan
+                Advantage::updateOrCreate(
+                    ['nama' => $judul, 'bahasa_id' => $bahasa],
+                    $keunggulanData
+                );
+            }
+        }
+
+        return redirect()->back()->with('success', 'Data berhasil diperbarui!');
+    }
+
+    public function switchVote($bahasaId)
+    {
+        // Ambil data berdasarkan bahasa_id
+        $vote = Vote::where('bahasa_id', $bahasaId)->pluck('isi', 'nama');
+
+        // Cek apakah data ditemukan
+        if ($vote->isEmpty()) {
+            return response()->json(['error' => 'Data tidak ditemukan'], 404);
+        }
+
+        return response()->json($vote);
+    }
+
+
+    // Penjadwalan
+    public function Penjadwalan()
+    {
+        $lang = Language::all();
+
+        $data = Schedule::where('bahasa_id', 1)
+            ->pluck('isi', 'nama');
+
+        // Ambil data kontak dengan bahasa default (misalnya bahasa dengan ID 1)
+        $bahasa_id = 1;
+
+        return view('Page_Editor.Sections.Produk.penjadwalan', compact('lang', 'bahasa_id', 'data'));
+    }
+
+
+    public function switchPenjadwalan($bahasaId)
+    {
+        // Ambil data berdasarkan bahasa_id
+        $schedule = Schedule::where('bahasa_id', $bahasaId)->pluck('isi', 'nama');
+
+        // Cek apakah data ditemukan
+        if ($schedule->isEmpty()) {
+            return response()->json(['error' => 'Data tidak ditemukan'], 404);
+        }
+
+        return response()->json($schedule);
+    }
+
+    public function storePenjadwalan(Request $request, $bahasa)
+    {
+        // dd($request->all());
+        $fields = ['Judul', 'Deskripsi', 'TeksButtonCoba', 'TeksButtonTutorial', 'Link', 'JudulBagianKeunggulan', 'KeteranganBagianKeunggulan'];
+
+        $data = ['nama' => 'penjadwalan'];
+
+        // Cek apakah ada gambar baru
+        if ($request->hasFile('foto_produk')) {
+            $file = $request->file('foto_produk');
+            $filenameToStore = 'penjadwalan' . time() . '.' . $file->getClientOriginalExtension();
+            $path = $file->storeAs('images/product', $filenameToStore, 'public');
+            $data['gambar'] = $path;
+        }
+
+        Product::updateOrCreate(['nama' => 'vote'], $data);
+
+        foreach ($fields as $field) {
+            $value = $request->input($field, '');
+
+            Schedule::updateOrCreate(
+                ['bahasa_id' => $bahasa, 'nama' => $field],
+                ['isi' => $value ?: '-']
+            );
+        }
+
+        return redirect()->back()->with('success', 'Data berhasil diperbarui!');
+    }
+
 }
---
+++ resources/views/Page_Editor/Sections/Produk/penjadwalan.blade.php
@@ -0,0 +1,307 @@
+@extends('dashboard')
+
+@section('title', 'Editor Halaman - Penjadwalan')
+
+@section('content')
+
+    <style>
+        .ck-editor__editable {
+            min-height: 250px;
+        }
+    </style>
+
+    <div class="container mt-4">
+
+        <div class="card p-3 shadow-sm">
+            <div class="row">
+                <div class="col-md">
+                    <h4 class="mt-2" style="color:blueviolet;">Editor Halaman</h4>
+                </div>
+
+                <div class="col-md-3 mt-1 text-end">
+                    <select class="form-select" id="pilihBahasa">
+                        @foreach ($lang as $item)
+                            <option value="{{ $item->id }}" {{ $bahasa_id == $item->id ? 'selected' : '' }}>
+                                Bahasa {{ $item->nama_bahasa }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <div class="card p-4 shadow-sm">
+
+            <div class="row mt-1">
+                <div class="col-md-9">
+                    <h5 class="mt-2">Penjadwalan - Bahasa</h5>
+                </div>
+            </div>
+
+            <hr>
+
+            <form id="formPenjadwalan" action="{{ route('update.penjadwalan', ['bahasa' => 1]) }}" method="post">
+                @csrf
+                @method('put')
+
+                <!-- Judul Halaman -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="judul" class="form-label mt-2 fw-bold">Judul halaman:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="judul" name="Judul" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['Judul'] ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Deskripsi Halaman -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="deskripsiEditor" class="form-label mt-2 fw-bold">Deskripsi halaman:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <textarea type="text" id="deskripsiEditor" name="Deskripsi" class="editor" placeholder="Ketik disini.....">{{ $data['Deskripsi'] ?? '' }}</textarea>
+                    </div>
+                </div>
+
+                <!-- Button Coba -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="teks_btncoba" class="form-label mt-2 fw-bold">Teks button coba:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="teks_btncoba" name="TeksButtonCoba" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['TeksButtonCoba'] ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Button Tutorial -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="teks_btntutor" class="form-label mt-2 fw-bold">Teks button tutorial:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="teks_btntutor" name="TeksButtonTutorial" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['TeksButtonTutorial'] ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Video Tutorial Voting -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="video_tutor" class="form-label mt-2 fw-bold">Link video tutorial:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="video_tutor" name="Link" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['Link'] ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Foto Produk -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="foto_produk" class="form-label mt-2 fw-bold">Foto produk:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="file" id="foto_produk" name="foto_produk" class="form-control" accept="image/*"
+                            >
+                    </div>
+                </div>
+
+                <hr>
+
+                <!-- Judul bagaian keunggulan -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="bag_keunggulan" class="form-label mt-2 fw-bold">Judul bagian keunggulan:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="bag_keunggulan" name="JudulBagianKeunggulan" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['JudulBagianKeunggulan'] ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Keterangan singkat keunggulan -->
+                <div class="row mb-4">
+                    <div class="col-md-3">
+                        <label for="ket_keunggulan" class="form-label mt-2 fw-bold">Keterangan singkat keunggulan:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <input type="text" id="ket_keunggulan" name="KeteranganBagianKeunggulan" class="form-control"
+                            placeholder="Ketik disini....." value="{{ $data['KeteranganBagianKeunggulan'] ?? '' }}"
+                            required>
+                    </div>
+                </div>
+
+                {{-- Keunggulan --}}
+                <div class="row mb-3 mt-2">
+                    <div class="col-md-3 mt-1">
+                        <label class="form-label fw-bold">Keunggulan produk:</label>
+                    </div>
+
+                    <div class="col-md">
+                        <button type="button" class="btn btn-success mb-1" id="tambahKeunggulanV"><i
+                                class="bi bi-plus"></i>
+                            Tambah Keunggulan
+                        </button>
+
+                        <small class="ms-2"><i class="bi bi-info-circle">Tambahkan ikon dengan rasio 1:1</i></small>
+
+                        <hr>
+
+                        <div id="listKeunggulanVote" class="mt-2">
+                            <div class="row mb-3 keunggulanV-item align-items-center">
+                                <!-- Kolom Input -->
+                                <div class="col-md-11">
+                                    <div class="row mb-2">
+                                        <div class="col-md">
+                                            <input type="file" name="keunggulanV_image[]" class="form-control"
+                                                accept="image/*">
+                                        </div>
+                                        <div class="col-md">
+                                            <input type="text" name="keunggulanV_judul[]" class="form-control"
+                                                placeholder="Judul Keunggulan">
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md">
+                                            <input type="text" name="keunggulanV_keterangan[]" class="form-control"
+                                                placeholder="Keterangan Keunggulan">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Kolom Button Hapus -->
+                                <div class="col-md-1 text-end">
+                                    <button type="button" class="btn btn-danger hapusKeunggulanV"><i
+                                            class="bi bi-trash"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Button Save -->
+                <div class="row">
+                    <div class="col-md-12 text-end">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+    <script>
+        $(document).ready(function() {
+
+            let editor;
+
+            ClassicEditor.create($("#deskripsiEditor")[0])
+                .then(newEditor => {
+                    editor = newEditor;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            $('#pilihBahasa').change(function() {
+                var bahasaId = $(this).val();
+                var form = $('#formPenjadwalan');
+
+                $.ajax({
+                    url: '/editor_halaman/penjadwalan/' + bahasaId,
+                    type: 'GET',
+                    success: function(response) {
+
+                        // Ubah action form agar menyertakan bahasaId
+                        form.attr('action', '/update_penjadwalan/' + bahasaId);
+
+                        if (response) {
+                            $('#judul').val(response.Judul || '');
+                            editor.setData(response.Deskripsi || '');
+                            $('#teks_btncoba').val(response.TeksButtonCoba || '');
+                            $('#teks_btntutor').val(response.TeksButtonTutorial || '');
+                            $('#video_tutor').val(response.Link || '');
+                            $('#bag_keunggulan').val(response.JudulBagianKeunggulan || '');
+                            $('#ket_keunggulan').val(response.KeteranganBagianKeunggulan || '');
+                        } else {
+                            $('#judul').val('');
+                            editor.setData('');
+                            $('#teks_btncoba').val('');
+                            $('#teks_btntutor').val('');
+                            $('#video_tutor').val('');
+                            $('#bag_keunggulan').val('');
+                            $('#ket_keunggulan').val('');
+                        }
+                    },
+                    error: function() {
+                        // Ubah action form agar menyertakan bahasaId
+                        form.attr('action', '/update_penjadwalan/' + bahasaId);
+
+                        $('#judul').val('');
+                            editor.setData('');
+                            $('#teks_btncoba').val('');
+                            $('#teks_btntutor').val('');
+                            $('#video_tutor').val('');
+                            $('#bag_keunggulan').val('');
+                            $('#ket_keunggulan').val('');
+                    }
+                });
+            });
+
+            // Tambah Keunggulan Produk Voting
+            $('#tambahKeunggulanV').click(function() {
+                var keunggulanBaru = `
+                <div class="row mb-3 keunggulanV-item align-items-center">
+                    <div class="col-md-11">
+                        <div class="row mb-2">
+                            <div class="col-md">
+                                <input type="file" name="keunggulanV_image[]" class="form-control" accept="image/*" required>
+                            </div>
+                            <div class="col-md">
+                                <input type="text" name="keunggulanV_judul[]" class="form-control" placeholder="Judul Keunggulan" required>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md">
+                                <input type="text" name="keunggulanV_keterangan[]" class="form-control" placeholder="Keterangan Keunggulan" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-1 text-end">
+                        <button type="button" class="btn btn-danger hapusKeunggulanV"><i class="bi bi-trash"></i></button>
+                    </div>
+                </div>
+            `;
+                $('#listKeunggulanVote').append(keunggulanBaru);
+            });
+
+            // Hapus Keunggulan Produk Voting
+            $(document).on('click', '.hapusKeunggulanV', function() {
+                $(this).closest('.keunggulanV-item').remove();
+            });
+
+            // Preview Nama File Setelah Upload
+            $('input[type="file"]').on('change', function() {
+                var fileName = $(this).val().split('\\').pop();
+                $(this).next('.custom-file-label').addClass("selected").html(fileName);
+            });
+        });
+    </script>
+
+@endsection
